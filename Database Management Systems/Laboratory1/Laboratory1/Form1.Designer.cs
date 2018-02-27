60c585f3a73b6f00b19363f42b45bc271d490f39
Andrei Olteanu <a.olteanu198@yahoo.com>
2018-02-27 19:35:13 +0200

    Database Management Systems Lab 1 start

---
+++ Database Management Systems/Laboratory1/Laboratory1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Laboratory1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.populateBtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // populateBtn
+            // 
+            this.populateBtn.Location = new System.Drawing.Point(511, 500);
+            this.populateBtn.Name = "populateBtn";
+            this.populateBtn.Size = new System.Drawing.Size(155, 55);
+            this.populateBtn.TabIndex = 0;
+            this.populateBtn.Text = "Populate";
+            this.populateBtn.UseVisualStyleBackColor = true;
+            this.populateBtn.Click += new System.EventHandler(this.populateBtn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 633);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(1146, 636);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1178, 1281);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.populateBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button populateBtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Database Management Systems/Laboratory1/Laboratory1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratory1
+{
+    public partial class Form1 : Form
+    {
+        // First we are creating a connection
+        SqlConnection connection = new SqlConnection("Data Source = OLTEANUANDR9310\\SQLEXPRESS; Initial Catalog = Foodster; Integrated Security = True");
+        SqlDataAdapter da = new SqlDataAdapter();
+        DataSet ds = new DataSet();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+           
+        }
+
+        private void populateBtn_Click(object sender, EventArgs e)
+        {
+            da.SelectCommand = new SqlCommand("SELECT * FROM ORDR", connection);
+            ds.Clear();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
