e437fa0f490bfe10fda9d9e1a4047ebe2d3d1bb4
Andrei Olteanu <a.olteanu198@yahoo.com>
2018-03-01 23:50:46 +0200

    2/4 tasks Done DMS Lab1

--- Database Management Systems/Laboratory1/Laboratory1/Form1.Designer.cs
+++ Database Management Systems/Laboratory1/Laboratory1/Form1.Designer.cs
@@ -28,49 +28,230 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.populateBtn = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.forCustomerLbl = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxCustomersFN = new System.Windows.Forms.TextBox();
+            this.textBoxCustomersLN = new System.Windows.Forms.TextBox();
+            this.textBoxCustomersAddress = new System.Windows.Forms.TextBox();
+            this.textBoxCustomersPhone = new System.Windows.Forms.TextBox();
+            this.populateBtnOrders = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxCustomersID = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // populateBtn
-            // 
-            this.populateBtn.Location = new System.Drawing.Point(511, 500);
-            this.populateBtn.Name = "populateBtn";
-            this.populateBtn.Size = new System.Drawing.Size(155, 55);
-            this.populateBtn.TabIndex = 0;
-            this.populateBtn.Text = "Populate";
-            this.populateBtn.UseVisualStyleBackColor = true;
-            this.populateBtn.Click += new System.EventHandler(this.populateBtn_Click);
             // 
             // dataGridView1
             // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 633);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 677);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 33;
-            this.dataGridView1.Size = new System.Drawing.Size(1146, 636);
+            this.dataGridView1.Size = new System.Drawing.Size(1086, 540);
             this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 608);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 51);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Customers";
+            // 
+            // forCustomerLbl
+            // 
+            this.forCustomerLbl.AutoSize = true;
+            this.forCustomerLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.forCustomerLbl.Location = new System.Drawing.Point(1107, 617);
+            this.forCustomerLbl.Name = "forCustomerLbl";
+            this.forCustomerLbl.Size = new System.Drawing.Size(159, 51);
+            this.forCustomerLbl.TabIndex = 3;
+            this.forCustomerLbl.Text = "Orders";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(1104, 677);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 33;
+            this.dataGridView2.Size = new System.Drawing.Size(1288, 540);
+            this.dataGridView2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 248);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "First Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(412, 313);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(155, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Phone Number";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(412, 236);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 25);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Address";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(24, 313);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 25);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Last Name";
+            // 
+            // textBoxCustomersFN
+            // 
+            this.textBoxCustomersFN.Location = new System.Drawing.Point(166, 245);
+            this.textBoxCustomersFN.Name = "textBoxCustomersFN";
+            this.textBoxCustomersFN.Size = new System.Drawing.Size(186, 31);
+            this.textBoxCustomersFN.TabIndex = 9;
+            // 
+            // textBoxCustomersLN
+            // 
+            this.textBoxCustomersLN.Location = new System.Drawing.Point(166, 310);
+            this.textBoxCustomersLN.Name = "textBoxCustomersLN";
+            this.textBoxCustomersLN.Size = new System.Drawing.Size(186, 31);
+            this.textBoxCustomersLN.TabIndex = 10;
+            // 
+            // textBoxCustomersAddress
+            // 
+            this.textBoxCustomersAddress.Location = new System.Drawing.Point(608, 230);
+            this.textBoxCustomersAddress.Name = "textBoxCustomersAddress";
+            this.textBoxCustomersAddress.Size = new System.Drawing.Size(210, 31);
+            this.textBoxCustomersAddress.TabIndex = 11;
+            // 
+            // textBoxCustomersPhone
+            // 
+            this.textBoxCustomersPhone.Location = new System.Drawing.Point(608, 313);
+            this.textBoxCustomersPhone.Name = "textBoxCustomersPhone";
+            this.textBoxCustomersPhone.Size = new System.Drawing.Size(210, 31);
+            this.textBoxCustomersPhone.TabIndex = 12;
+            // 
+            // populateBtnOrders
+            // 
+            this.populateBtnOrders.Location = new System.Drawing.Point(1842, 611);
+            this.populateBtnOrders.Name = "populateBtnOrders";
+            this.populateBtnOrders.Size = new System.Drawing.Size(160, 48);
+            this.populateBtnOrders.TabIndex = 13;
+            this.populateBtnOrders.Text = "Populate";
+            this.populateBtnOrders.UseVisualStyleBackColor = true;
+            this.populateBtnOrders.Click += new System.EventHandler(this.populateBtnOrders_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1104, 427);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(189, 51);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Add new Order";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(1039, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(243, 61);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Foodster";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(24, 185);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(32, 25);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "ID";
+            // 
+            // textBoxCustomersID
+            // 
+            this.textBoxCustomersID.Location = new System.Drawing.Point(166, 185);
+            this.textBoxCustomersID.Name = "textBoxCustomersID";
+            this.textBoxCustomersID.Size = new System.Drawing.Size(186, 31);
+            this.textBoxCustomersID.TabIndex = 17;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1178, 1281);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(2402, 1229);
+            this.Controls.Add(this.textBoxCustomersID);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.populateBtnOrders);
+            this.Controls.Add(this.textBoxCustomersPhone);
+            this.Controls.Add(this.textBoxCustomersAddress);
+            this.Controls.Add(this.textBoxCustomersLN);
+            this.Controls.Add(this.textBoxCustomersFN);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.forCustomerLbl);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.populateBtn);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button populateBtn;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label forCustomerLbl;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxCustomersFN;
+        private System.Windows.Forms.TextBox textBoxCustomersLN;
+        private System.Windows.Forms.TextBox textBoxCustomersAddress;
+        private System.Windows.Forms.TextBox textBoxCustomersPhone;
+        private System.Windows.Forms.Button populateBtnOrders;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBoxCustomersID;
     }
 }
 
--- Database Management Systems/Laboratory1/Laboratory1/Form1.cs
+++ Database Management Systems/Laboratory1/Laboratory1/Form1.cs
@@ -15,19 +15,17 @@ namespace Laboratory1
     {
         // First we are creating a connection
         SqlConnection connection = new SqlConnection("Data Source = OLTEANUANDR9310\\SQLEXPRESS; Initial Catalog = Foodster; Integrated Security = True");
+
         SqlDataAdapter da = new SqlDataAdapter();
         DataSet ds = new DataSet();
+
+        SqlDataAdapter da1 = new SqlDataAdapter();
+        DataSet ds1 = new DataSet();
 
         public Form1()
         {
             InitializeComponent();
-
-           
-        }
-
-        private void populateBtn_Click(object sender, EventArgs e)
-        {
-            da.SelectCommand = new SqlCommand("SELECT * FROM ORDR", connection);
+            da.SelectCommand = new SqlCommand("SELECT * FROM USR", connection);
             ds.Clear();
             da.Fill(ds);
             dataGridView1.DataSource = ds.Tables[0];
@@ -35,7 +33,67 @@ namespace Laboratory1
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            int selectedIndex = e.RowIndex;
 
+            // Need to check this for pressing the header and getting index out of range error.
+            if (e.RowIndex == -1) return;
+
+            this.getDataForSelectedRowsOrCells(selectedIndex);
+            this.displayChildRecords(selectedIndex);
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int selectedIndex = e.RowIndex;
+            
+            // Need to check this for pressing the header and getting index out of range error.
+            if (e.RowIndex == -1) return;
+
+            this.getDataForSelectedRowsOrCells(selectedIndex);
+            this.displayChildRecords(selectedIndex);
+
+
+        }
+
+        // Displays all the orders taken by a certain customer, by ID
+        private void displayChildRecords(int forCustomerID)
+        {
+            da1.SelectCommand = new SqlCommand("SELECT * FROM ORDR WHERE UID = @id", connection);
+            da1.SelectCommand.Parameters.AddWithValue("@id", getCustomerID(forCustomerID));
+            ds1.Clear();
+            da1.Fill(ds1);
+            dataGridView2.DataSource = ds1.Tables[0];
+        }
+
+        // Returns the ID of the customer which was pressed
+        string getCustomerID(int forCustomerID)
+        {
+            DataGridViewRow selectedRow = dataGridView1.Rows[forCustomerID];
+            forCustomerLbl.Text = "Orders for " + selectedRow.Cells[1].Value.ToString() + " " + selectedRow.Cells[2].Value.ToString();
+            return selectedRow.Cells[0].Value.ToString();
+        }
+
+
+        // Gets the data from a selected index and displays it in the textBoxes
+        private void getDataForSelectedRowsOrCells(int selectedIndex)
+        {
+            DataGridViewRow selectedRow = dataGridView1.Rows[selectedIndex];
+
+            textBoxCustomersID.Text = selectedRow.Cells[0].Value.ToString();
+            textBoxCustomersFN.Text = selectedRow.Cells[1].Value.ToString();
+            textBoxCustomersLN.Text = selectedRow.Cells[2].Value.ToString();
+            textBoxCustomersAddress.Text = selectedRow.Cells[3].Value.ToString();
+            textBoxCustomersPhone.Text = selectedRow.Cells[4].Value.ToString();
+        }
+
+        // Populates with all the orders
+        private void populateBtnOrders_Click(object sender, EventArgs e)
+        {
+            da1.SelectCommand = new SqlCommand("SELECT * FROM ORDR", connection);
+            ds1.Clear();
+            da1.Fill(ds1);
+            dataGridView2.DataSource = ds1.Tables[0];
+            forCustomerLbl.Text = "Orders";
         }
     }
 }
