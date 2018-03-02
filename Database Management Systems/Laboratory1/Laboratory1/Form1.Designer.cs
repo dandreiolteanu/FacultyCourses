acaf2c569ae02b68e3cd1e5a4818be25b7646f6e
Andrei Olteanu <a.olteanu198@yahoo.com>
2018-03-02 02:25:29 +0200

    3/4 Done for Laboratory 1

--- Database Management Systems/Laboratory1/Laboratory1/Form1.Designer.cs
+++ Database Management Systems/Laboratory1/Laboratory1/Form1.Designer.cs
@@ -45,6 +45,21 @@
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.textBoxCustomersID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.updateOrderBtn = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.checkBoxCash = new System.Windows.Forms.CheckBox();
+            this.checkBoxCreditCard = new System.Windows.Forms.CheckBox();
+            this.textBoxOrderOID = new System.Windows.Forms.TextBox();
+            this.textBoxOrderRestaurant = new System.Windows.Forms.TextBox();
+            this.textBoxOrderUID = new System.Windows.Forms.TextBox();
+            this.textBoxOrderTotal = new System.Windows.Forms.TextBox();
+            this.textBoxOrderTimeStamp = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
@@ -92,6 +107,8 @@
             this.dataGridView2.RowTemplate.Height = 33;
             this.dataGridView2.Size = new System.Drawing.Size(1288, 540);
             this.dataGridView2.TabIndex = 4;
+            this.dataGridView2.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellClick);
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
             // 
             // label3
             // 
@@ -202,12 +219,159 @@
             this.textBoxCustomersID.Size = new System.Drawing.Size(186, 31);
             this.textBoxCustomersID.TabIndex = 17;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1104, 198);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 25);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "OID";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(1723, 248);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(47, 25);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "UID";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(1104, 251);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(126, 25);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Time Stamp";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(1104, 319);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(150, 25);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "Payment Type";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(1723, 195);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(60, 25);
+            this.label12.TabIndex = 22;
+            this.label12.Text = "Total";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(1723, 316);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(117, 25);
+            this.label13.TabIndex = 23;
+            this.label13.Text = "Restaurant";
+            // 
+            // updateOrderBtn
+            // 
+            this.updateOrderBtn.Location = new System.Drawing.Point(1572, 427);
+            this.updateOrderBtn.Name = "updateOrderBtn";
+            this.updateOrderBtn.Size = new System.Drawing.Size(189, 51);
+            this.updateOrderBtn.TabIndex = 25;
+            this.updateOrderBtn.Text = "Update Order";
+            this.updateOrderBtn.UseVisualStyleBackColor = true;
+            this.updateOrderBtn.Click += new System.EventHandler(this.updateOrderBtn_Click);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(1972, 427);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(189, 51);
+            this.deleteBtn.TabIndex = 26;
+            this.deleteBtn.Text = "Delete Order";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // checkBoxCash
+            // 
+            this.checkBoxCash.AutoSize = true;
+            this.checkBoxCash.Location = new System.Drawing.Point(1292, 318);
+            this.checkBoxCash.Name = "checkBoxCash";
+            this.checkBoxCash.Size = new System.Drawing.Size(94, 29);
+            this.checkBoxCash.TabIndex = 27;
+            this.checkBoxCash.Text = "Cash";
+            this.checkBoxCash.UseVisualStyleBackColor = true;
+            this.checkBoxCash.Click += new System.EventHandler(this.chk_Click);
+            // 
+            // checkBoxCreditCard
+            // 
+            this.checkBoxCreditCard.AutoSize = true;
+            this.checkBoxCreditCard.Location = new System.Drawing.Point(1431, 319);
+            this.checkBoxCreditCard.Name = "checkBoxCreditCard";
+            this.checkBoxCreditCard.Size = new System.Drawing.Size(153, 29);
+            this.checkBoxCreditCard.TabIndex = 28;
+            this.checkBoxCreditCard.Text = "Credit Card";
+            this.checkBoxCreditCard.UseVisualStyleBackColor = true;
+            this.checkBoxCreditCard.Click += new System.EventHandler(this.chk_Click);
+
+            // 
+            // textBoxOrderOID
+            // 
+            this.textBoxOrderOID.Location = new System.Drawing.Point(1292, 192);
+            this.textBoxOrderOID.Name = "textBoxOrderOID";
+            this.textBoxOrderOID.Size = new System.Drawing.Size(292, 31);
+            this.textBoxOrderOID.TabIndex = 29;
+            // 
+            // textBoxOrderRestaurant
+            // 
+            this.textBoxOrderRestaurant.Location = new System.Drawing.Point(1869, 310);
+            this.textBoxOrderRestaurant.Name = "textBoxOrderRestaurant";
+            this.textBoxOrderRestaurant.Size = new System.Drawing.Size(292, 31);
+            this.textBoxOrderRestaurant.TabIndex = 30;
+            // 
+            // textBoxOrderUID
+            // 
+            this.textBoxOrderUID.Location = new System.Drawing.Point(1869, 245);
+            this.textBoxOrderUID.Name = "textBoxOrderUID";
+            this.textBoxOrderUID.Size = new System.Drawing.Size(292, 31);
+            this.textBoxOrderUID.TabIndex = 31;
+            // 
+            // textBoxOrderTotal
+            // 
+            this.textBoxOrderTotal.Location = new System.Drawing.Point(1869, 189);
+            this.textBoxOrderTotal.Name = "textBoxOrderTotal";
+            this.textBoxOrderTotal.Size = new System.Drawing.Size(292, 31);
+            this.textBoxOrderTotal.TabIndex = 32;
+            // 
+            // textBoxOrderTimeStamp
+            // 
+            this.textBoxOrderTimeStamp.Location = new System.Drawing.Point(1292, 248);
+            this.textBoxOrderTimeStamp.Name = "textBoxOrderTimeStamp";
+            this.textBoxOrderTimeStamp.Size = new System.Drawing.Size(292, 31);
+            this.textBoxOrderTimeStamp.TabIndex = 33;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(2402, 1229);
+            this.Controls.Add(this.textBoxOrderTimeStamp);
+            this.Controls.Add(this.textBoxOrderTotal);
+            this.Controls.Add(this.textBoxOrderUID);
+            this.Controls.Add(this.textBoxOrderRestaurant);
+            this.Controls.Add(this.textBoxOrderOID);
+            this.Controls.Add(this.checkBoxCreditCard);
+            this.Controls.Add(this.checkBoxCash);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.updateOrderBtn);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.textBoxCustomersID);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
@@ -252,6 +416,21 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.TextBox textBoxCustomersID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button updateOrderBtn;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.CheckBox checkBoxCash;
+        private System.Windows.Forms.CheckBox checkBoxCreditCard;
+        private System.Windows.Forms.TextBox textBoxOrderOID;
+        private System.Windows.Forms.TextBox textBoxOrderRestaurant;
+        private System.Windows.Forms.TextBox textBoxOrderUID;
+        private System.Windows.Forms.TextBox textBoxOrderTotal;
+        private System.Windows.Forms.TextBox textBoxOrderTimeStamp;
     }
 }
 
--- Database Management Systems/Laboratory1/Laboratory1/Form1.cs
+++ Database Management Systems/Laboratory1/Laboratory1/Form1.cs
@@ -16,20 +16,90 @@ namespace Laboratory1
         // First we are creating a connection
         SqlConnection connection = new SqlConnection("Data Source = OLTEANUANDR9310\\SQLEXPRESS; Initial Catalog = Foodster; Integrated Security = True");
 
+        // Adapter and DataSet
         SqlDataAdapter da = new SqlDataAdapter();
         DataSet ds = new DataSet();
 
+        // Adapter and DataSet
         SqlDataAdapter da1 = new SqlDataAdapter();
         DataSet ds1 = new DataSet();
 
+        int OID;
+        
         public Form1()
         {
             InitializeComponent();
-            da.SelectCommand = new SqlCommand("SELECT * FROM USR", connection);
-            ds.Clear();
-            da.Fill(ds);
-            dataGridView1.DataSource = ds.Tables[0];
+            this.displayCustomers();
         }
+
+        // Deletes a record from Orders and displays the new data
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (OID != 0)
+                {
+                    connection.Open();
+                    da1.DeleteCommand = new SqlCommand("DELETE FROM OrderToDishes WHERE OID = @id", connection);
+                    da1.DeleteCommand.Parameters.AddWithValue("@id", this.OID);
+                    da1.DeleteCommand.ExecuteNonQuery();
+                    da1.DeleteCommand = new SqlCommand("DELETE FROM ORDR WHERE OID = @id", connection);
+                    da1.DeleteCommand.Parameters.AddWithValue("@id", this.OID);
+                    da1.DeleteCommand.ExecuteNonQuery();
+
+                    MessageBox.Show("Order Deleted Successfully!");
+                    this.displayOrders();
+                    this.ClearOrderTextboxData();
+                }
+                else
+                {
+                    MessageBox.Show("Please Select Record to Delete");
+                }
+
+            } catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            } finally
+            {
+                connection.Close();
+            }
+            
+        }
+
+        private void updateOrderBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connection.Open();
+                da1.UpdateCommand = new SqlCommand("UPDATE ORDR SET OID = @oid, timeStamp = @timeStamp, paymentType = @paymentType, total = @total, UID = @uid, RID = @rid WHERE OID = @oid", connection);
+                da1.UpdateCommand.Parameters.AddWithValue("@oid", this.OID);
+                da1.UpdateCommand.Parameters.AddWithValue("@timeStamp", textBoxOrderTimeStamp.Text);
+                if (checkBoxCash.Checked == true)
+                {
+                    da1.UpdateCommand.Parameters.AddWithValue("@paymentType", checkBoxCash.Text);
+                } else
+                {
+                    da1.UpdateCommand.Parameters.AddWithValue("@paymentType", checkBoxCreditCard.Text);
+                }
+                da1.UpdateCommand.Parameters.AddWithValue("@total", Convert.ToInt32(textBoxOrderTotal.Text));
+                da1.UpdateCommand.Parameters.AddWithValue("@UID", Convert.ToInt32(textBoxOrderUID.Text));
+                da1.UpdateCommand.Parameters.AddWithValue("@RID", textBoxOrderRestaurant.Text);
+                da1.UpdateCommand.ExecuteNonQuery();
+
+                MessageBox.Show("Order Updated Successfully!");
+                this.displayOrders();
+                this.ClearOrderTextboxData();
+
+            } catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            } finally
+            {
+                connection.Close();
+            }
+        }
+
+
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
@@ -38,8 +108,9 @@ namespace Laboratory1
             // Need to check this for pressing the header and getting index out of range error.
             if (e.RowIndex == -1) return;
 
-            this.getDataForSelectedRowsOrCells(selectedIndex);
+            this.populateCustomerTextboxesFromIndex(selectedIndex);
             this.displayChildRecords(selectedIndex);
+            this.ClearOrderTextboxData();
         }
 
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
@@ -49,11 +120,22 @@ namespace Laboratory1
             // Need to check this for pressing the header and getting index out of range error.
             if (e.RowIndex == -1) return;
 
-            this.getDataForSelectedRowsOrCells(selectedIndex);
+            this.populateCustomerTextboxesFromIndex(selectedIndex);
             this.displayChildRecords(selectedIndex);
+            this.ClearOrderTextboxData();
 
 
         }
+
+        // Displays all the parents records
+        private void displayCustomers()
+        {
+            da.SelectCommand = new SqlCommand("SELECT * FROM USR", connection);
+            ds.Clear();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
 
         // Displays all the orders taken by a certain customer, by ID
         private void displayChildRecords(int forCustomerID)
@@ -75,7 +157,7 @@ namespace Laboratory1
 
 
         // Gets the data from a selected index and displays it in the textBoxes
-        private void getDataForSelectedRowsOrCells(int selectedIndex)
+        private void populateCustomerTextboxesFromIndex(int selectedIndex)
         {
             DataGridViewRow selectedRow = dataGridView1.Rows[selectedIndex];
 
@@ -86,14 +168,98 @@ namespace Laboratory1
             textBoxCustomersPhone.Text = selectedRow.Cells[4].Value.ToString();
         }
 
+        private void populateOrderTextboxesFromIndex(int selectedIndex)
+        {
+            DataGridViewRow selectedRow = dataGridView2.Rows[selectedIndex];
+
+            textBoxOrderOID.Text = selectedRow.Cells[0].Value.ToString();
+            textBoxOrderTimeStamp.Text = selectedRow.Cells[1].Value.ToString();
+            textBoxOrderTotal.Text = selectedRow.Cells[3].Value.ToString();
+            textBoxOrderUID.Text = selectedRow.Cells[4].Value.ToString();
+            textBoxOrderRestaurant.Text = selectedRow.Cells[5].Value.ToString();
+
+            string paymentType = selectedRow.Cells[2].Value.ToString();
+            if (paymentType == "Cash")
+            {
+                checkBoxCash.Checked = true;
+                checkBoxCreditCard.Checked = false;
+                lastChecked = checkBoxCash;
+            }
+            else
+            {
+                checkBoxCash.Checked = false;
+                checkBoxCreditCard.Checked = true;
+                lastChecked = checkBoxCreditCard;
+            }
+            try
+            {
+                this.OID = Convert.ToInt32(selectedRow.Cells[0].Value.ToString());
+            }
+            catch (Exception ex)
+            {
+                new ApplicationException("Select a valid Row!");
+            }
+
+        }
+
         // Populates with all the orders
         private void populateBtnOrders_Click(object sender, EventArgs e)
         {
+            this.displayOrders();
+        }
+
+        // Function that displays all the orders
+        private void displayOrders()
+        {
+            //connection.Open();
             da1.SelectCommand = new SqlCommand("SELECT * FROM ORDR", connection);
             ds1.Clear();
             da1.Fill(ds1);
             dataGridView2.DataSource = ds1.Tables[0];
             forCustomerLbl.Text = "Orders";
+            //connection.Close();
+        }
+
+        private void dataGridView2_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int selectedIndex = e.RowIndex;
+
+            // Need to check this for pressing the header and getting index out of range error.
+            if (e.RowIndex == -1) return;
+
+            this.populateOrderTextboxesFromIndex(selectedIndex);
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int selectedIndex = e.RowIndex;
+
+            // Need to check this for pressing the header and getting index out of range error.
+            if (e.RowIndex == -1) return;
+
+            this.populateOrderTextboxesFromIndex(selectedIndex);
+        }
+
+        // Clears the data
+        private void ClearOrderTextboxData()
+        {
+            textBoxOrderOID.Text = "";
+            textBoxOrderTimeStamp.Text = "";
+            textBoxOrderTotal.Text = "";
+            textBoxOrderUID.Text = "";
+            textBoxOrderRestaurant.Text = "";
+            checkBoxCash.Checked = false;
+            checkBoxCreditCard.Checked = false;
+            OID = 0;
+        }
+
+        // Helper, for the CheckBoxes. Allowing only one at a time.
+        CheckBox lastChecked;
+        private void chk_Click(object sender, EventArgs e)
+        {
+            CheckBox activeCheckBox = sender as CheckBox;
+            if (activeCheckBox != lastChecked && lastChecked != null) lastChecked.Checked = false;
+            lastChecked = activeCheckBox.Checked ? activeCheckBox : null;
         }
     }
 }
