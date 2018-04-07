5464e8f164e3327e5d7433b295e74cdbef95b94b
Andrei Olteanu <a.olteanu198@yahoo.com>
2018-04-07 16:57:36 +0300

    Lab 5 done

--- WebProgramming/Lab5/lab5.js
+++ WebProgramming/Lab5/lab5.js
@@ -1,11 +1,11 @@
-$(function(){
-    $(".small-images").hover(function(e){
-        var imageSrc = $(this).attr("src")
-        var imageName = $(this).attr("alt")
-        var href = $(this).attr("href");
-        $(".big-image img").attr("src", imageSrc);
-        $(".big-image h3").html(imageName)
-        e.preventDefault();
-        return false;
-    });
+$(function() {
+  $(".small-images").hover(function(e) {
+    var imageSrc = $(this).attr("src");
+    var imageName = $(this).attr("alt");
+    var href = $(this).attr("href");
+    $(".big-image img").attr("src", imageSrc);
+    $(".big-image h3").html(imageName);
+    e.preventDefault();
+    return false;
+  });
 });
