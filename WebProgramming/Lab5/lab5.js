9b8a32ec37a9da1f6136e08119484201ab367e35
Andrei Olteanu <a.olteanu198@yahoo.com>
2018-04-17 15:13:22 +0300

    Done lab3

--- WebProgramming/Lab5/lab5.js
+++ WebProgramming/Lab5/lab5.js
@@ -1,11 +1,22 @@
-$(function() {
-  $(".small-images").hover(function(e) {
-    var imageSrc = $(this).attr("src");
-    var imageName = $(this).attr("alt");
-    var href = $(this).attr("href");
-    $(".big-image img").attr("src", imageSrc);
-    $(".big-image h3").html(imageName);
-    e.preventDefault();
-    return false;
-  });
-});
+// JQUERY
+// $(function() {
+//   $(".small-images").hover(function(e) {
+//     var imageSrc = $(this).attr("src");
+//     var imageName = $(this).attr("alt");
+//     var href = $(this).attr("href");
+//     $(".big-image img").attr("src", imageSrc);
+//     $(".big-image h3").html(imageName);
+//     e.preventDefault();
+//     return false;
+//   });
+// });
+
+// JAVASCRIPT
+function change(imgTag) {
+  var imgSrc = imgTag.getAttribute('src');
+  var imgName = imgTag.getAttribute('alt');
+  var bigImage = document.getElementById('bigImage');
+  bigImage.src = imgSrc;
+  var headerBigImage = document.getElementById('bigImageTitle');
+  headerBigImage.textContent = imgName
+}
